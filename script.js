af4ccc76d13f32b96cbb2ffb3abd9ea3db1592e6
richard mejias <richard987@hotmail.cl>
2024-01-12 13:51:33 -0300

    trabajo terminado

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-function celsiusFahrenheit(celsius) {
-    return (celsius * 9/5 ) + 32;
-}
+//function celsiusFahrenheit(celsius) {
+//    return (celsius * 9/5 ) + 32;
+//}
 
-function celsiusKelvin(celsius) {
-    return celsius + 273.15;
-}
+//function celsiusKelvin(celsius) {
+//    return celsius + 273.15;
+//}
+
+function factorial(num) {
+    if (num === 0 || num === 1) {
+      return 1;
+    } else {
+      return num * factorial(num - 1);
+    }
+  }
 
 function conversor() {
-    let tempCelsius = parseFloat(document.getElementById("celsiusInput").value);
+    let numfact = parseInt(document.getElementById("factInput").value);
 
-    if (!isNaN(tempCelsius)) {
-        let tempFahrenheit = celsiusFahrenheit(tempCelsius).toFixed(2);
-        let tempKelvin = celsiusKelvin(tempCelsius).toFixed(2);
+    if (!isNaN(numfact)) {
+        let fact = factorial(numfact).toFixed(2);
+        //let tempKelvin = celsiusKelvin(tempCelsius).toFixed(2);
 
         let resultadosDiv = document.getElementById("resultados");
-        resultadosDiv.innerHTML = "<p>" + tempCelsius + " grados Celsius son " + tempFahrenheit + " grados Fahrenheit.</p>";
-        resultadosDiv.innerHTML += "<p>" + tempCelsius + " grados Celsius son " + tempKelvin + " Kelvin.</p>";
+        resultadosDiv.innerHTML = "<p>" + " El factorial de " + numfact + " es " + fact + "</p>";
+        //resultadosDiv.innerHTML += "<p>" + tempCelsius + " grados Celsius son " + tempKelvin + " Kelvin.</p>";
 
     } else {
-        alert("Por favor, ingrese una temperatura válida.");
+        alert("Error, por favor ingrese un número ya que el conversor solo acepta números como entradas.");
     }
 }
